dbc95baf4a3bb7c4ebd0b77a418d9900118ba686
Swizec Teller <swizec@swizec.com>
2020-06-16 08:22:20 -0700

    Async data load

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import * as d3 from "d3";
+import _ from "lodash";
 
 import "./App.css";
 import Preloader from "./components/Preloader";
+import { loadAllData } from "./DataHandling";
 
 function App() {
     const [techSalaries, setTechSalaries] = useState([]);
+    const [medianIncomes, setMedianIncomes] = useState([]);
+    const [countyNames, setCountyNames] = useState([]);
+
+    async function loadData() {
+        const data = await loadAllData();
+
+        const { techSalaries, medianIncomes, countyNames } = data;
+
+        setTechSalaries(techSalaries);
+        setMedianIncomes(medianIncomes);
+        setCountyNames(countyNames);
+    }
+
+    useEffect(() => {
+        loadData();
+    }, []);
 
     if (techSalaries.length < 1) {
         return <Preloader />;
     } else {
-        return <div className="App"></div>;
+        return (
+            <div className="App container">
+                <h1>Loaded {techSalaries.length} salaries</h1>
+            </div>
+        );
     }
 }
 
