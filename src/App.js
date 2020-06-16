1c1ba48d7ccfb9ce6b5590a5ae7bd735e92a5633
Swizec Teller <swizec@swizec.com>
2020-06-16 07:23:54 -0700

    Preloader

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+import "./App.css";
+import Preloader from "./components/Preloader";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [techSalaries, setTechSalaries] = useState([]);
+
+    if (techSalaries.length < 1) {
+        return <Preloader />;
+    } else {
+        return <div className="App"></div>;
+    }
 }
 
 export default App;
