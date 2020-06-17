b5f073216cf8c3f5e2f024584d1acb97bb0c143d
Swizec Teller <swizec@swizec.com>
2020-06-16 22:46:06 -0700

    CountyMap optimized with useMemo

--- src/components/CountyMap.js
+++ src/components/CountyMap.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 import * as d3 from "d3";
 import * as topojson from "topojson";
 import _ from "lodash";
@@ -28,6 +28,50 @@ const County = ({ geoPath, feature, zoom, key, quantize, value }) => {
     );
 };
 
+function useQuantize(values) {
+    return useMemo(() => {
+        const scale = d3.scaleQuantize().range(d3.range(9));
+
+        if (values) {
+            scale.domain([
+                d3.quantile(values, 0.15, (d) => d.value),
+                d3.quantile(values, 0.85, (d) => d.value),
+            ]);
+        }
+
+        return scale;
+    }, [values]);
+}
+
+function useProjection({ width, height, zoom, usTopoJson, USstateNames }) {
+    return useMemo(() => {
+        const projection = d3
+            .geoAlbersUsa()
+            .scale(1280)
+            .translate([width / 2, height / 2])
+            .scale(width * 1.3);
+        const geoPath = d3.geoPath().projection(projection);
+
+        if (zoom && usTopoJson) {
+            const us = usTopoJson,
+                USstatePaths = topojson.feature(us, us.objects.states).features,
+                id = _.find(USstateNames, { code: zoom }).id;
+
+            projection.scale(width * 4.5);
+
+            const centroid = geoPath.centroid(_.find(USstatePaths, { id: id })),
+                translate = projection.translate();
+
+            projection.translate([
+                translate[0] - centroid[0] + width / 2,
+                translate[1] - centroid[1] + height / 2,
+            ]);
+        }
+
+        return geoPath;
+    }, [width, height, zoom, usTopoJson, USstateNames]);
+}
+
 const CountyMap = ({
     usTopoJson,
     USstateNames,
@@ -38,36 +82,14 @@ const CountyMap = ({
     zoom,
     values,
 }) => {
-    const projection = d3
-        .geoAlbersUsa()
-        .scale(1280)
-        .translate([width / 2, height / 2])
-        .scale(width * 1.3);
-    const geoPath = d3.geoPath().projection(projection);
-    const quantize = d3.scaleQuantize().range(d3.range(9));
-
-    if (zoom && usTopoJson) {
-        const us = usTopoJson,
-            USstatePaths = topojson.feature(us, us.objects.states).features,
-            id = _.find(USstateNames, { code: zoom }).id;
-
-        projection.scale(width * 4.5);
-
-        const centroid = geoPath.centroid(_.find(USstatePaths, { id: id })),
-            translate = projection.translate();
-
-        projection.translate([
-            translate[0] - centroid[0] + width / 2,
-            translate[1] - centroid[1] + height / 2,
-        ]);
-    }
-
-    if (values) {
-        quantize.domain([
-            d3.quantile(values, 0.15, (d) => d.value),
-            d3.quantile(values, 0.85, (d) => d.value),
-        ]);
-    }
+    const geoPath = useProjection({
+        width,
+        height,
+        zoom,
+        usTopoJson,
+        USstateNames,
+    });
+    const quantize = useQuantize(values);
 
     if (!usTopoJson) {
         return null;
